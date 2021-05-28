6a61ae6708d960ce8250b16dd7fec0b8fe7d65b8
hojoon <hj2758@gmail.com>
2021-05-28 16:55:54 +0900

    검색할 페이지 수

--- project/ga.py
+++ project/ga.py
@@ -15,7 +15,17 @@ list_all = []
 def f_get_list(item, page):
     url = 'http://shop.danawa.com/virtualestimate/?controller=estimateMain&methods=product&categorySeq='
     url += item
-    url += '&categoryDepth=2&marketPlaceSeq=16&sellerSeq=0&pseq=2&name=&listPerPage=30&attribute=&makerCode=&brandCode=&serviceSectionSeq=0&productRegisterAreaGroupSeq=0&ignoreKeywordYN=N&preparationSale=&tabOrderbyYn=N&suggestName=&displayOptionCount=3&categoryName=%EA%B7%B8%EB%9E%98%ED%94%BD%EC%B9%B4%EB%93%9C&productRegisterAreaSeq=4&categorySeq1=861&categorySeq2=876&categorySeq3=0&categorySeq4=0&selectOptionList[]=654&selectOptionList[]=655&selectOptionList[]=6857&selectOptionList[]=658&selectOptionList[]=657&selectOptionList[]=659&selectOptionList[]=6858&selectOptionList[]=665&selectOptionList[]=661&selectOptionList[]=663&selectOptionList[]=664&selectOptionList[]=6201&selectOptionList[]=6574&selectOptionList[]=680&selectOptionList[]=16333&selectOptionList[]=20550&selectOptionList[]=31689&selectOptionList[]=7599&selectOptionList[]=321985&selectOptionList[]=684&selectOptionList[]=666&selectOptionList[]=682&selectOptionList[]=32778&selectOptionList[]=37388&selectOptionList[]=32182&selectOptionList[]=321958&goodsCount=916&page='
+    url += '''&categoryDepth=2&marketPlaceSeq=16&sellerSeq=0&pseq=2&name=&listPerPage=30
+    &attribute=&makerCode=&brandCode=&serviceSectionSeq=0&productRegisterAreaGroupSeq=0
+    &ignoreKeywordYN=N&preparationSale=&tabOrderbyYn=N&suggestName=&displayOptionCount=3
+    &categoryName=%EA%B7%B8%EB%9E%98%ED%94%BD%EC%B9%B4%EB%93%9C&productRegisterAreaSeq=4
+    &categorySeq1=861&categorySeq2=876&categorySeq3=0&categorySeq4=0&selectOptionList[]=654
+    &selectOptionList[]=655&selectOptionList[]=6857&selectOptionList[]=658&selectOptionList[]=657
+    &selectOptionList[]=659&selectOptionList[]=6858&selectOptionList[]=665&selectOptionList[]=661
+    &selectOptionList[]=663&selectOptionList[]=664&selectOptionList[]=6201&selectOptionList[]=6574
+    &selectOptionList[]=680&selectOptionList[]=16333&selectOptionList[]=20550&selectOptionList[]=31689
+    &selectOptionList[]=7599&selectOptionList[]=321985&selectOptionList[]=684&selectOptionList[]=666
+    &selectOptionList[]=682&selectOptionList[]=32778&selectOptionList[]=37388&selectOptionList[]=32182&selectOptionList[]=321958&goodsCount=916&page='''
     url += str(page)
     driver = webdriver.Chrome('./chromedriver')
     driver.get(url)
@@ -33,16 +43,15 @@ def f_get_list(item, page):
 
     return item_list
 
-
+# 검색할 페이지
+searchPage = 3
 # 아이템, 페이지 지정해서 상품 상세보기 링크 리스트에 저장 (다차원 배열)
-for i in range(1, 26):
+for i in range(1, searchPage):
     list_all.append(f_get_list(memory, i))
 
-# print(list_all)
+print(list_all)
 
 length = len(list_all)
-
-# print(length)
 
 # 1차원 배열로 저장
 list_url = []
