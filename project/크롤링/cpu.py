55b828a83714c79650d5df30076fc487c2066824
hojoon <hj2758@gmail.com>
2021-05-30 01:56:51 +0900

    create cpu table

--- project/크롤링/cpu.py
+++ project/크롤링/cpu.py
@@ -8,5 +8,28 @@ from selenium.webdriver.chrome.options import Options
 conn = sql.connect('./db/cpu.db')
 cur = conn.cursor()
 
+cur.execute('''
+create table CPU (
+cpu_id varchar2(20) primary key ,
+cpu_name varchar2(100) not null ,
+cpu_category varchar2(20) not null ,
+cpu_price number default 0 ,
+cpu_year date  ,
+cpu_img varchar2(100)  ,
+cpu_detail_img varchar2(100)  ,
+cpu_maker varchar2(100)  ,
+cpu_type varchar2(50)  ,
+cpu_socket varchar2(50)  ,
+cpu_mm number  ,
+cpu_core number  ,
+cpu_thread number  ,
+cpu_clock float(20)  ,
+cpu_bit number  ,
+cpu_memory_bus number  ,
+cpu_gpu varchar2(10)  ,
+cpu_codename varchar2(50)  
+)
+''')
+
 
 
