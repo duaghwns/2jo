a323b45416cbce75f7052df9218c19bd72950139
hojoon <hj2758@gmail.com>
2021-05-29 19:45:19 +0900

    csv 파일 불러와서 리스트에 다시 담기

---
+++ project/크롤링/test.py
@@ -0,0 +1,11 @@
+import csv
+
+# csv 파일 불러와서 리스트에 다시 담기
+url = []
+with open('test_url_list.csv','r',encoding='utf-8') as f:
+    rdr = csv.reader(f)
+    for i,line in enumerate(rdr):
+        if i==0:
+            url = line
+
+print(url)
